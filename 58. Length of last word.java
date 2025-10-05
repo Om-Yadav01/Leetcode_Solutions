5f021951853ce8887a92d475ee732aa27728ea88
Om Yadav <007omyadav1@gmail.com>
2025-10-06 02:44:46 +0530

    Create 58. Length of last word.java

---
+++ 58. Length of last word.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int res = 0;
+        String[] part = s.split(" ");
+        int n = part.length;
+        //System.out.print(n);
+        res = part[part.length-1].length();
+        return res;
+    }
+}
