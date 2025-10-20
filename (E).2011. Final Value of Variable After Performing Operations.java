27bfc1bae90058ed5bca10b3cd4f3f731b6174b8
Om Yadav <007omyadav1@gmail.com>
2025-10-20 09:26:03 +0530

    Implement finalValueAfterOperations method

---
+++ (E).2011. Final Value of Variable After Performing Operations.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int count=0;
+        for(int i=0;i<operations.length;i++){
+           if(operations[i].charAt(1)=='+' || operations[i].charAt(0)=='+') count++;
+           else count--;
+        }
+        return count;
+    }
+}
